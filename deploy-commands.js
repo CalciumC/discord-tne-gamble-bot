fa323a54ada2db61604c05d2a2d6e8639522bf16
CalciumC <chancalcium@gmail.com>
2022-02-18 14:44:49 +0800

    migrate from replit, add dependency: dotenv

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
@@ -8,8 +10,9 @@ const GUILD_ID = process.env['SERVER_ID'];
 
 const commands = [
   new SlashCommandBuilder().setName('currentgame').setDescription('Show the current game.'),
-  new SlashCommandBuilder().setName('balance').setDescription('Show user balance')
-    .addUserOption(option => option.setName('user').setDescription('The user')),
+  new SlashCommandBuilder().setName('leaderboard').setDescription('Show the leaderboard.'),
+  new SlashCommandBuilder().setName('balance').setDescription('Show user balance.')
+    .addUserOption(option => option.setName('user').setDescription('The user.')),
 ].map(command => command.toJSON()); 
 
 const rest = new REST({ version: '9' }).setToken(TOKEN);
